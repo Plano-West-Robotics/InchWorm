0a2cb76009a7f4c0d210b0d364aee396dd9138e5
Yash Karandikar <yash@karx.xyz>
2023-05-14 16:22:04 -0500

    Add docs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/inchworm/units/Angle.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/inchworm/units/Angle.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.inchworm.units;
 
 public class Angle {
+    /** 0 degrees/radians, use for convenience */
     public static final Angle ZERO = new Angle(0);
     // value in radians
     private final double val;
@@ -9,18 +10,36 @@ public class Angle {
         val = v;
     }
 
+    /**
+     * Create an angle from radians
+     * @param ang Angle in radians
+     * @return Angle object containing the given angle
+     */
     public static Angle radians(double ang) {
         return new Angle(ang);
     }
 
+    /**
+     * Create an angle from degrees
+     * @param ang Angle in degrees
+     * @return Angle object containing the given angle
+     */
     public static Angle degrees(double ang) {
         return new Angle(Math.toRadians(ang));
     }
 
+    /**
+     * Returns this angle in radians
+     * @return Angle in radians
+     */
     public double angleInRadians() {
         return val;
     }
 
+    /**
+     * Returns this angle in degrees
+     * @return Angle in degrees
+     */
     public double angleInDegrees() {
         return Math.toDegrees(val);
     }
@@ -43,8 +62,8 @@ public class Angle {
 
     /**
      * Returns the real (smallest) difference between two angles.
-     * @param a first angle (in radians)
-     * @param b second angle (in radians)
+     * @param a first angle
+     * @param b second angle
      * @return smallest difference between the two angles, within range [-π, π)
      */
     public static Angle sub(Angle a, Angle b) {
@@ -54,10 +73,20 @@ public class Angle {
         return Angle.radians(diff);
     }
 
+    /**
+     * Returns the sum of two angles
+     * @param a first angle
+     * @param b second angle
+     * @return sum of the two angles
+     */
     public static Angle add(Angle a, Angle b) {
         return Angle.radians(a.angleInRadians() + b.angleInRadians());
     }
 
+    /**
+     * Returns the negative of this angle.
+     * @return This angle multiplied by -1
+     */
     public Angle neg() {
         return Angle.radians(-val);
     }
